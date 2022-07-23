84be6893a433fe88d7c6679e285ba5cfb0443c8a
HeberTU <heber.trujillo@glovoapp.com>
2022-07-23 21:08:55 +0200

    feat: flask app.

---
+++ app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Flask app file.
+
+Created on: 23/7/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+### Temporary database
+### We will replace this dictionary
+database = {
+    'users': {"username": "heber",
+              "password": "admin123"}
+}
+logged_in = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    """Login endpoint."""
+    if request.method == 'GET':
+        return render_template('form.html', title='Sign In')
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == database.get("users").get(
+                'username') and password == database.get('users').get(
+                'password'):
+
+            logged_in = True
+
+            return "<h1>Hi {0}, You are successfully logged in.</h1>".format(
+                username)
+        else:
+
+            return " <h2>Wrong Credentials! Please <a href=\'/\'>try again</a></h2>"
